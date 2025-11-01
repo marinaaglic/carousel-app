ee2bfd165fe48e3bc23b9acb884192386255bc45
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-11-01 09:58:51 +0100

    imported all images to one file

---
+++ utils/Images.ts
@@ -0,0 +1,28 @@
+import First from '../public/image1.jpg'
+import Second from '../public/image2.jpg'
+import Third from '../public/image3.jpg'
+import Fourth from '../public/image4.jpg'
+import Fifth from '../public/image5.jpg'
+
+export default [
+  {
+    title: 'First slide',
+    url: First,
+  },
+  {
+    title: 'Second slide',
+    url: Second,
+  },
+  {
+    title: 'Third slide',
+    url: Third,
+  },
+  {
+    title: 'Fourth slide',
+    url: Fourth,
+  },
+  {
+    title: 'Fifth slide',
+    url: Fifth,
+  },
+]
