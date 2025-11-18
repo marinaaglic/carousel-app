d7c5ea61f6046acb5e89fbcec48148b5115651fe
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-11-18 12:26:04 +0100

    added functions for navigating slider

--- components/ImageCarousel/ImageCarousel.tsx
+++ components/ImageCarousel/ImageCarousel.tsx
@@ -1,7 +1,29 @@
 'use client'
 import CarouselControls from '../CarouselControls/CarouselControls.tsx'
 import { useState } from 'react'
+import { Slides } from '@/utils/SlideData.ts'
 
 export default function ImageCarousel() {
-  return <div></div>
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % Slides.length)
+  }
+
+  const handlePrevious = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + Slides.length) % Slides.length
+    )
+  }
+
+  if (currentIndex > Slides.length - 3) {
+    console.log(currentIndex)
+    setCurrentIndex(0)
+    console.log('hi')
+  }
+  return (
+    <div>
+      <CarouselControls onPrevClick={handlePrevious} onNextClick={handleNext} />
+    </div>
+  )
 }
