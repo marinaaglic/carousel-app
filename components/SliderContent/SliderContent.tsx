c35a49b4e3bc7b4e30e99f6e8e229bd799a558c3
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-11-19 16:13:15 +0100

    deleted a folder

--- components/SliderContent/SliderContent.tsx
+++ components/SliderContent/SliderContent.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { ImageProps } from 'next/image'
 import { Slides } from '../../utils/SlideData.ts'
 import ImageWrapper from '../reusable/ImageWrapper.tsx'
 
-interface SliderContentProps {
-  currentIndex: number
-}
+export default function SliderContent() {
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
 
-export default function SliderContent({ currentIndex }: SliderContentProps) {
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % Slides.length)
+  }
+
+  const handlePrevious = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + Slides.length) % Slides.length
+    )
+  }
   const slide = Slides[currentIndex]
   return (
     <div>
